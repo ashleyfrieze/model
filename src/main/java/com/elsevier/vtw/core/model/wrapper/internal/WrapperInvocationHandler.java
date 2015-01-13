3abfb2566d89958f9f4fd203ae6f8507df82c573
ashleyfrieze <ashley@incredible.org.uk>
2015-01-13 13:48:36 +0000

    Refactored and added caching to support multiple requests on the same type.

--- src/main/java/com/elsevier/vtw/core/model/wrapper/internal/WrapperInvocationHandler.java
+++ src/main/java/com/elsevier/vtw/core/model/wrapper/internal/WrapperInvocationHandler.java
@@ -1,8 +1,9 @@
 package com.elsevier.vtw.core.model.wrapper.internal;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -13,41 +14,53 @@ public class WrapperInvocationHandler<T> implements InvocationHandler {
 	private ObjectNode jsonData;
 	private Class<T> wrappedType;
 	
+	private static Map<Method, InvocationDefinition> invocationDefinitions = new HashMap<>();
+	private static Object invocationDefinitionsLock = new Object();
+	
 	private static final String GETTER_PREFIX = "get";
 	private static final String SETTER_PREFIX = "set";
 	
-	
+	/**
+	 * How to handle the invocation
+	 */
 	interface HandlingStrategy {
 		Object handle(Object[] parameters);
 	}
 	
-	class InvocationDefinition {
+	/**
+	 * Instructions on what a given method means in terms of choosing invocation strategy
+	 */
+	static class InvocationDefinition {
 		String fieldName;
 		boolean isGetter;
 		boolean isSetter;
 	}
 	
+	/**
+	 * Constructor
+	 * @param wrappedType the type that is being proxied
+	 * @param jsonData the place where the json data is being stored/wrapped
+	 */
 	public WrapperInvocationHandler(Class<T> wrappedType, ObjectNode jsonData) {
 		this.wrappedType = wrappedType;
 		this.jsonData = jsonData;
 	}
 	
 	@Override
-	public Object invoke(Object arg0, Method arg1, Object[] arg2)
+	public Object invoke(Object arg0, Method method, Object[] arg2)
 			throws Throwable {
-		HandlingStrategy strategy = findInvocationStrategy(arg1);
+		HandlingStrategy strategy = findInvocationStrategy(method);
 		if (strategy!=null) {
 			return strategy.handle(arg2);
-		} if (arg1.equals(Wrapper.class.getMethod("json"))) {
+		} if (method.equals(Wrapper.class.getMethod("json"))) {
 			return jsonData;
 		} else {
 			throw new UnsupportedOperationException("This method cannot be called");
 		}
 	}
 
-	private HandlingStrategy findInvocationStrategy(Method arg1) throws Throwable {
-		
-		InvocationDefinition definition = getInvocationDefinition(arg1);
+	private HandlingStrategy findInvocationStrategy(Method method) throws Throwable {
+		InvocationDefinition definition = getInvocationDefinition(method);
 		if (definition!=null) {
 			if (definition.isGetter) {
 				return createStringGetter(definition.fieldName);
@@ -59,46 +72,63 @@ public class WrapperInvocationHandler<T> implements InvocationHandler {
 		return null;
 	}
 
-	private InvocationDefinition getInvocationDefinition(Method arg1) {
-		Field annotation = getFieldAnnotation(arg1);
+	private InvocationDefinition getInvocationDefinition(Method method) {
+		synchronized(invocationDefinitionsLock) {
+			// look it up if possible
+			InvocationDefinition def = invocationDefinitions.get(method);
+			if (def!=null) {
+				return def;
+			}
+			
+			// build and cache the invocation definition
+			def = buildInvocationDefinition(method);
+			invocationDefinitions.put(method, def);
+			return def;
+		}
+	}
+
+	private InvocationDefinition buildInvocationDefinition(Method method) {
+		Field annotation = getFieldAnnotation(method);
 		if(annotation!=null) {
 			InvocationDefinition def = new InvocationDefinition();
 			def.fieldName = annotation.value();
-			def.isGetter = arg1.getName().startsWith(GETTER_PREFIX);
-			def.isSetter = arg1.getName().startsWith(SETTER_PREFIX);
+			def.isGetter = method.getName().startsWith(GETTER_PREFIX);
+			def.isSetter = method.getName().startsWith(SETTER_PREFIX);
 			return def;
 		}
 		return null;
 	}
 
-	private Field getFieldAnnotation(Method arg1) {
-		Field field = getFieldAnnotationFrom(arg1);
+	/**
+	 * Find the {link:Field} annotation on this method or its sibling if a getter/setter
+	 * @param method the method to review
+	 * @return {link:Field} annotation from this method, if available, or its sibling if not
+	 */
+	private Field getFieldAnnotation(Method method) {
+		Field field = getFieldAnnotationFrom(method);
 		if (field!=null) {
 			return field;
 		}
 		
 		// perhaps this is a getter/setter where the other one has the
 		// annotation
-		if (arg1.getName().startsWith(GETTER_PREFIX)) {
-			return getFieldAnnotationForRePrefixed(arg1, SETTER_PREFIX);
+		if (method.getName().startsWith(GETTER_PREFIX)) {
+			return getFieldAnnotationForRePrefixed(method, SETTER_PREFIX);
 		}
-		if (arg1.getName().startsWith(SETTER_PREFIX)) {
-			return getFieldAnnotationForRePrefixed(arg1, GETTER_PREFIX);
+		if (method.getName().startsWith(SETTER_PREFIX)) {
+			return getFieldAnnotationForRePrefixed(method, GETTER_PREFIX);
 		}
 		
 		return null;
 	}
 
-	private Field getFieldAnnotationFrom(Method arg1) {
-		if (arg1.isAnnotationPresent(Field.class)) {
-			return arg1.getAnnotation(Field.class);
-		}
-		return null;
+	private Field getFieldAnnotationFrom(Method method) {
+		return method.getAnnotation(Field.class);
 	}
 
-	private Field getFieldAnnotationForRePrefixed(Method arg1, String otherPrefix) {
+	private Field getFieldAnnotationForRePrefixed(Method method, String otherPrefix) {
 		Method[] methods = wrappedType.getMethods();
-		String newName = otherPrefix + arg1.getName().substring(SETTER_PREFIX.length());
+		String newName = otherPrefix + method.getName().substring(SETTER_PREFIX.length());
 		for(Method possibleMatch:methods) {
 			if (possibleMatch.getName().equals(newName)) {
 				return getFieldAnnotationFrom(possibleMatch);
